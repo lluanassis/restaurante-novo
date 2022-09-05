881bcd42468d12bf884001890f3e9619299f585a
Luan Assis <luan2lobo@gmail.com>
2022-09-04 22:59:25 -0300

    navbar 2

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,42 +1,80 @@
-import React from "react";
-import { GiHamburguerMenu } from "react-icons/gi";
+import React, { useState } from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
 
 import images from "../../constants/images";
 import "./Navbar.css";
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className="app__navbar-logo">
-      <img src={images.gericht} alt="app logo" />
-    </div>
-    <ul className="app__navbar-links">
-      <li className="p__opensans">
-        <a href="#home">Home</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#sobre">Sobre</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#menu">Menu</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#premios">Prêmios</a>
-      </li>
-      <li className="p__opensans">
-        <a href="#contato">Contato</a>
-      </li>
-    </ul>
-    <div className="app__navbar-login">
-      <a href="#login" className="p__opensans">
-        Acessar / Registrar-me
-      </a>
-      <div/>
-      <a href="/" className="p__opensans">
-        Cardápio
-      </a>
-    </div>
-  </nav>
-);
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  return (
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={images.gericht} alt="app logo" />
+      </div>
+      <ul className="app__navbar-links">
+        <li className="p__opensans">
+          <a href="#home">Home</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#sobre">Sobre</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#menu">Menu</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#premios">Prêmios</a>
+        </li>
+        <li className="p__opensans">
+          <a href="#contato">Contato</a>
+        </li>
+      </ul>
+      <div className="app__navbar-login">
+        <a href="#login" className="p__opensans">
+          Acessar / Registrar-me
+        </a>
+        <div />
+        <a href="/" className="p__opensans">
+          Cardápio
+        </a>
+      </div>
+      <div className="app__navbar-smallscreen">
+        <GiHamburgerMenu
+          color="#fff"
+          fontSize={27}
+          onClick={() => setToggleMenu(true)}
+        />
+
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu
+              fontSize={27}
+              className="overlay__close"
+              onClick={() => setToggleMenu(false)}
+            />
+            <ul className="app__navbar-smallscreen_links">
+              <li className="p__opensans">
+                <a href="#inicio">Início</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#sobre">Sobre</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#menu">Menu</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#premios">Prêmios</a>
+              </li>
+              <li className="p__opensans">
+                <a href="#contato">Contato</a>
+              </li>
+            </ul>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
